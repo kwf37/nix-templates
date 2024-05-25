c2ea3a77e402add6ff8a354cc38b3998c641be1f
Kenneth Fang <fangkw42@gmail.com>
2024-05-25 01:52:16 -0400

    remove vscode from template to avoid bulk

--- templates/frontend/flake.nix
+++ templates/frontend/flake.nix
@@ -34,23 +34,10 @@
           name = "Frontend development shell";
 
           packages = with pkgs; 
-            let vscodeWithExtensions =
-              vscode-with-extensions.override {
-                vscodeExtensions = with vscode-extensions; [
-                  bbenoist.nix
-                  vscodevim.vim
-                  dbaeumer.vscode-eslint
-                  esbenp.prettier-vscode
-                  formulahendry.auto-rename-tag
-                  yzhang.markdown-all-in-one
-                ];
-              };
-            in
-              [ 
-                vscodeWithExtensions 
-                nodejs-18_x
-                nodePackages.pnpm
-              ];
+            [ 
+              nodejs-18_x
+              nodePackages.pnpm
+            ];
           };
       });
     };
